cf4c19f6f7fbf0a5e97c585226911f28052dcf9d
jayrpl <jadenpalmerleandre@hotmail.co.uk>
2024-11-30 22:52:58 +0000

    added Display question, get menu selection and method to change difficulty. choo choo.

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -11,4 +11,70 @@ Random random = new Random();
 
 int firstNumber;
 int secondNumber;
+int userMenuSelection;
+int score = 0;
+bool gameOver = false;
 
+static DifficultyLevel ChangeDifficulty()
+{
+  int userSelection = 0;
+  bool correctInput = false;
+
+  Console.WriteLine("Please enter a difficulty number.");
+  Console.WriteLine("1 - Easy");
+  Console.WriteLine("2 - Medium");
+  Console.WriteLine("3 - Hard");
+
+  correctInput = int.TryParse(Console.ReadLine(), out userSelection);
+
+  while (!correctInput)
+  {
+    Console.WriteLine("Please select a valid option.");
+  }
+
+  switch (userSelection)
+  {
+    case 1:
+      return DifficultyLevel.Easy;
+    case 2:
+      return DifficultyLevel.Medium;
+    case 3:
+      return DifficultyLevel.Hard;
+  }
+
+  return DifficultyLevel.Easy;
+}
+
+static void DisplayMathGameQuestion(int firstNumber, int secondNumber, char operation)
+{
+  Console.WriteLine($"{firstNumber} {operation} {secondNumber} = ?");
+}
+
+static int GetMenuSelection(MathGameLogic mathGame)
+{
+  int menuSelection = -1;
+  mathGame.BuildMenu();
+
+  while (menuSelection < 1 || menuSelection > 7)
+  {
+    while (!int.TryParse(Console.ReadLine(), out menuSelection))
+    {
+      Console.WriteLine("Please select a valid option 1-8");
+    }
+  }
+
+  return menuSelection;
+
+}
+
+static void PerformOperation(char operation)
+{
+
+}
+
+public enum DifficultyLevel
+{
+  Easy = 45,
+  Medium = 30,
+  Hard = 15
+}
